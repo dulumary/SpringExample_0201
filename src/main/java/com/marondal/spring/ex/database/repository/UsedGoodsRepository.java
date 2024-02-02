3557e0fc0b289fb599b36ff6a6e6402a74b84db2
megait <megait@gmail.com>
2024-02-02 20:33:38 +0900

    [김인규]  - spring mvc 예제 진행

---
+++ src/main/java/com/marondal/spring/ex/database/repository/UsedGoodsRepository.java
@@ -0,0 +1,16 @@
+package com.marondal.spring.ex.database.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.marondal.spring.ex.database.domain.UsedGoods;
+
+// Mybatis framework
+// 데이터베이스 관련 기능 담당
+@Mapper
+public interface UsedGoodsRepository {
+	
+	public List<UsedGoods> selectUsedGoodsList();
+
+}
---
+++ src/main/java/com/marondal/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,27 @@
+package com.marondal.spring.ex.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.ex.database.domain.UsedGoods;
+import com.marondal.spring.ex.database.repository.UsedGoodsRepository;
+
+// 로직(business) 담당
+@Service
+public class UsedGoodsService {
+	
+	@Autowired
+	private UsedGoodsRepository usedGoodsRepository;
+	
+	// 중고물품 게시글 리스트 
+	public List<UsedGoods> getUsedGoodsList() {
+		
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		
+		return usedGoodsList;
+		
+	}
+
+}
