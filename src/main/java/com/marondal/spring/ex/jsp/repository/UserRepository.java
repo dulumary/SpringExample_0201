27483211a1e037222f6215c6645a04d079ebc059
megait <megait@gmail.com>
2024-02-07 21:47:27 +0900

    [김인규]  - insert 페이지, form 연계 예제

---
+++ src/main/java/com/marondal/spring/ex/jsp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.marondal.spring.ex.jsp.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+	
+	 public int insertUser(
+			 @Param("name") String name
+			 , @Param("birthday") String birthday
+			 , @Param("email") String email);
+
+}
